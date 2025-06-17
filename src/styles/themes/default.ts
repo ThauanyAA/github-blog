b9abd3938882728e872c7041ab4fb6c2919f3c34
thauanyASN <thauanya@trustsecurenow.com>
2025-06-17 18:16:34 -0300

    Starting project - initial structure

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  colors: {
+    white: '#ffffff',
+    primary: '#8257e6',
+    secondary: '#04d361',
+    background: '#121214',
+    text: '#e1e1e6',
+    border: '#202024',
+    inputBackground: '#202024',
+    inputText: '#c4c4cc',
+  },
+  fonts: {
+    body: 'Roboto, sans-serif',
+    heading: 'Poppins, sans-serif',
+  },
+  breakpoints: {
+    mobile: '768px',
+    tablet: '1024px',
+    desktop: '1440px',
+  }
+}
